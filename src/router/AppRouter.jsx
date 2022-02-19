c042ae74bb77fb13e014de5420ca3b37c9813bc8
Alejandro <silvestroalejandro@gmail.com>
2022-02-19 18:30:56 -0300

    Agrego proteccion de rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
     BrowserRouter as Router,
     Switch,
@@ -10,11 +10,15 @@ import {
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { startChecking } from '../actions/auth';
+// import { PublicRoute } from './PublicRoute';
+// import { PrivateRoute } from './PrivateRoute';
 
 
 export const AppRouter = () => {
 
     const dispatch = useDispatch();
+
+    const {checking, uid} = useSelector(state => state.auth);
 
     useEffect(() => {
 
@@ -22,13 +26,26 @@ export const AppRouter = () => {
 
     }, [dispatch]);
 
+    if (checking) {
+        return (<h5>Espere, cargando...</h5>);
+    }
+
     return (
         <Router>
             <div>
             <Switch>
-                <Route exact path="/login" component={ LoginScreen } />
-
-                <Route exact path="/" component={ CalendarScreen } />
+                <Route
+                    exact
+                    path="/login"
+                    component={ LoginScreen }
+                    isAuthenticated={!!uid}
+                />
+                <Route
+                    exact
+                    path="/"
+                    component={ CalendarScreen }
+                    isAuthenticated={!!uid}
+                    />
 
                 <Redirect to="/" />
             </Switch>
